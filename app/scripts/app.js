86c3cdeb485635f455bec57d5de91aef9c474c40
mathildeps <mathilde.steffansen@gmail.com>
2016-04-27 19:05:07 +0200

    Now with contact page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,45 +4,91 @@ var app = angular.module('store', ['ngRoute']);
 app.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/productpage', {
-        templateUrl: 'pages/products.html',
+      when('/home', {
+        templateUrl: 'pages/productpage.html',
         controller: 'StoreController'
       }).
       when('/basket/:productId', {
         templateUrl: 'pages/basket.html',
         controller: 'PhoneDetailCtrl'
       }).
+        when('/contact', {
+        templateUrl: 'pages/contact.html',
+        controller: 'FormController'
+      }).
       otherwise({
         redirectTo: '/Home'
       });
+}]);
 
 
-
-
-var gem = [{
+var daily = [{
 	name: 'Citizen Eco-Drive Mens Watch',
 	price: 260.90,
-	description: 'Watch for men',
-  image: 'http://shop.msf.dk/images/9136-brun-l%C3%A6derrem-dame.png'
+  image: 'images/ur.png'
 },
 {
 	name: 'Mens Faux-Leather Bomber Jacket',
 	price: 120.00,
-	description: 'Very Nice jacket',
-   image: 'http://shop.msf.dk/images/9136-brun-l%C3%A6derrem-dame.png'
+   image: 'images/bumper.png'
 },
 {
 	name: 'Dell Inspiron 13.3 Touchscreen Laptop',
 	price: 478.90,
-	description: 'Good tablet for you',
-   image: 'http://shop.msf.dk/images/9136-brun-l%C3%A6derrem-dame.png'
+   image: 'images/tablet.png'
+},
+{
+  name: 'Panasonic Lumix DMC-GH4',
+  price: 250.00,
+   image: 'images/kamera.png'
 }];
 
-app.controller('StoreController', function(){
-	this.products = gem;
+var popular = [{
+  name: 'Nike Mercurial Superfly IV FG',
+  price: 299.99,
+  image: 'images/sko.png'
+},
+{
+  name: 'JZ CHIEF brand Designer Women Bag',
+  price: 49.90,
+   image: 'images/taske.png'
+},
+{
+  name: 'Samsung Gear VR Virtual Reality',
+  price: 120.00,
+   image: 'images/virtuel.png'
+},
+{
+  name: 'Rayban Italy Sunglasses',
+  price: 250.00,
+   image: 'images/sol.png'
+}];
+
+app.controller('StoreController', function($scope){
+
+  $scope.basket = [];
+
+	this.products = daily;
+  this.popularproducts = popular;
+
+  $scope.putIntoBasket = function(prod) {
+    $scope.basket.push(prod);
+
+    console.log($scope.basket);
+  }
+
 });
 
 
+
+app.controller('FormController', function($scope) {
+  $scope.contactform = {};
+
+  $scope.sendform = function() {
+    $scope.contactform = {};
+    console.log("Sådan");
+  }
+});
 
 })();
 
